87de9b8f3cfea355128c73a1043f095e8c14ce0a
Kaushik Chakraborty <jayadrath@gmail.com>
2022-01-16 00:11:53 +0530

    First commit

---
+++ scripts/run.js
@@ -0,0 +1,47 @@
+const main = async () => {
+    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+    const waveContract = await waveContractFactory.deploy({
+        value: hre.ethers.utils.parseEther("0.1"),
+    });
+    await waveContract.deployed();
+    console.log("Contract deployed to: ", waveContract.address);
+
+    let contractBalance = await hre.ethers.provider.getBalance(
+        waveContract.address
+    );
+
+    console.log( "Contract balance:",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
+
+    let waveTxn = await waveContract.wave("No wave!");
+    await waveTxn.wait();
+
+    waveTxn = await waveContract.wave("Another wave!");
+    await waveTxn.wait();
+
+    contractBalance = await hre.ethers.provider.getBalance(
+        waveContract.address
+    );
+
+    console.log( "Contract balance:",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
+
+
+    let allWaves = await waveContract.getAllWaves();
+    console.log(allWaves);
+};
+
+const runMain = async() => {
+    try{
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+        
+    }
+};
+
+runMain();
